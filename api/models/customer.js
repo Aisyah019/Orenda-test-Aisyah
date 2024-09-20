fda8774afd643846a4689cfd67ee28d4beadcf2c
Aisyah019 <aisyerd09@gmail.com>
2024-09-20 09:40:06 +0700

    first commit

---
+++ api/models/customer.js
@@ -0,0 +1,32 @@
+const { DataTypes, Model } = require("sequelize");
+const sequelize = require("../config/database");
+
+const Customer = sequelize.define("customer", {
+    name: {
+        type: DataTypes.STRING(100), // Set maximum length to 100 characters
+        allowNull: false,
+    },
+    phone: {
+        type: DataTypes.STRING(15), // Set maximum length to 15 characters for phone numbers
+        allowNull: false,
+    },
+    email: {
+        type: DataTypes.STRING(100), // Set maximum length to 100 characters
+        allowNull: false,
+        unique: true,
+        validate: {
+            isEmail: true,
+        },
+    },
+    address: {
+        type: DataTypes.STRING(255), // Set maximum length to 255 characters
+        allowNull: false,
+    },
+},
+    {
+        timestamps: false,
+    }
+);
+
+
+module.exports = Customer;
